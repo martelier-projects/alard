ee23fd105f42244d44686e751be569e70d8ab775
Martijn Nieuwenhuizen <martijnnieuwenhuizen@icloud.com>
2021-08-20 14:21:31 +0200

    Update OG image

--- components/partials/Seo/index.tsx
+++ components/partials/Seo/index.tsx
@@ -2,5 +2,20 @@ import { NextSeo } from 'next-seo'
 import type SeoInterface from '../../../interfaces/Seo'
 
 export default function Seo({ pageTitle, pageDescription }: SeoInterface) {
-  return <NextSeo title={pageTitle} description={pageDescription} />
+  return (
+    <NextSeo
+      title={pageTitle}
+      description={pageDescription}
+      openGraph={{
+        site_name: 'Studenten verleiden tot leren',
+        title: pageTitle,
+        description: pageDescription,
+        images: [
+          {
+            url: '/images/boek_cover.png',
+          },
+        ],
+      }}
+    />
+  )
 }
