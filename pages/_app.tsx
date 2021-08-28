999902442250190f60ee6aad1a283b10b0b77135
Martijn Nieuwenhuizen <martijnnieuwenhuizen@icloud.com>
2021-08-28 11:10:28 +0200

    Add GA

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,46 @@
+import ReactGA from 'react-ga'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+
 import type { AppProps } from 'next/app'
 
 import '../styles/globals.scss'
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+
+  useEffect(() => {
+    if (!process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID) return
+
+    // Initialize GA.
+    const debug = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_DEBUG === 'true'
+    ReactGA.initialize(process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID, {
+      debug,
+    })
+    ReactGA.set({ anonymizeIp: true })
+
+    // Send Pageview for initial view.
+    const url = window.location.pathname + window.location.search
+    ReactGA.pageview(url)
+  }, [])
+
+  /**
+   * Send pageView for each page visited by client-side routing.
+   */
+  useEffect(() => {
+    if (!process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID) return () => {}
+
+    const handleRouteChange = (url: string) => {
+      ReactGA.pageview(url)
+    }
+
+    // Subscribe to router changes.
+    router.events.on('routeChangeComplete', handleRouteChange)
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return <Component {...pageProps} />
 }
